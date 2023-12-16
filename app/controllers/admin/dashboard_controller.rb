d596baae9a7b1d2667e1849d09dad78b37950041
Ana <anmeuri@gmail.com>
2023-12-15 16:22:39 -0800

    Add basic HTTP auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,16 @@
 class Admin::DashboardController < ApplicationController
+  before_action :authenticate
+
   def show
     @products_count = Product.all.count
     @categories_count = Category.all.count
+  end
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['HTTP_BASIC_AUTH_USERNAME'] && password == ENV['HTTP_BASIC_AUTH_PASSWORD']
+    end
   end
 end
