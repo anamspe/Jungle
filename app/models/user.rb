efad0cf4261316914addf41714770fe006ddd8f4
Ana <anmeuri@gmail.com>
2023-12-18 21:46:27 -0800

    Create User class method for auth to be used in sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,25 @@ class User < ApplicationRecord
   
   has_secure_password
 
+  def self.authenticate_with_credentials(email, password)
+    user = find_by_email(email.strip.downcase)
+    unless user
+      puts "No user with this email was found."
+    end
+
+    user if user && user.authenticate(password)
+  end
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, if: :password
+  
+  validate :password_confirmation_match, if: :password
+
+  def password_confirmation_match
+    errors.add(:password_confirmation, "doesn't match Password") if password != password_confirmation
+  end
+
 end
