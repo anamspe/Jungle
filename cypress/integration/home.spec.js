e813549a288f00af7ba2c7963c14f5d82d212c89
Ana <anmeuri@gmail.com>
2023-12-19 19:12:23 -0800

    Add tests to check access to homepage and products from test database

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,14 @@
+describe('Home page', () => {
+  beforeEach(() => {
+    cy.visit('localhost:3000');
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+})
